47492f0b9a32e9f2960da8dbf3339a486703d603
HABIBALLAH <hamza1habiballah@gmail.com>
2021-11-03 17:42:25 +0100

    Post method

--- src/main/java/service/EtudiantService.java
+++ src/main/java/service/EtudiantService.java
@@ -1,4 +1,38 @@
 package service;
 
+import dao.etudiant.EtudiantDao;
+import dao.etudiant.EtudiantDaoImpl;
+import entities.Etudiant;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.sql.SQLException;
+
+@Path("/etudiants")
 public class EtudiantService {
+
+    EtudiantDao etudiantDaoImpl = new EtudiantDaoImpl();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAllEtudiant() throws SQLException {
+        return etudiantDaoImpl.getAll();
+    }
+
+    @GET
+    @Path("/competences/{competences}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAllEtudiant(@PathParam("competences") String competences) throws SQLException {
+        return etudiantDaoImpl.getByCompetences(competences);
+    }
+
+    @POST
+    @Path("/save")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public Etudiant saveStudent(final Etudiant newStudent) throws SQLException {
+        System.out.println("called");
+        etudiantDaoImpl.save(new Etudiant(newStudent.getNom(), newStudent.getPrenom(), newStudent.getMail(), newStudent.getPassword(),newStudent.getNoEtudiant(), newStudent.getInscrit(), newStudent.getFormation(), newStudent.getLinkedInLink(), newStudent.getDescription(), newStudent.getCompetences(),null));
+        return newStudent;
+    }
 }
--- src/main/java/service/OffreService.java
+++ src/main/java/service/OffreService.java
@@ -6,13 +6,7 @@ import entities.Offre;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.sql.SQLException;
-
-@XmlRootElement
-class MyJaxBean { @XmlElement
-public String param1; @XmlElement public String param2; }
 
 @Path("/offres")
 public class OffreService {
@@ -20,23 +14,17 @@ public class OffreService {
     OffreDao offreDaoImpl = new OffreDaoImpl();
 
     @GET
-    @Path("/all")
     @Produces(MediaType.APPLICATION_JSON)
     public String getAllOffre() throws SQLException {
         return offreDaoImpl.getAll();
     }
 
     @GET
-    @Path("/all/{competences}")
+    @Path("/competences/{competences}")
     @Produces(MediaType.APPLICATION_JSON)
     public String getAllOffre(@PathParam("competences") String competences) throws SQLException {
         return offreDaoImpl.getByCompetences(competences);
     }
-//    @POST
-//    @Path("/save/{titre}/{contenu}/{competences}/{entrepriseName}")
-//    public void saveOffre(@PathParam("titre")String titre,@PathParam("contenu")String contenu,@PathParam("competences")String competences,@PathParam("entrepriseName")String entrepriseName) throws SQLException {
-//        offreDaoImpl.save(new Offre(titre,null,contenu,competences,entrepriseName));
-//    }
 
     @POST
     @Path("/save")
@@ -46,13 +34,5 @@ public class OffreService {
         System.out.println("called");
         offreDaoImpl.save(new Offre(newOffre.getTitre(), newOffre.getConfirme(), newOffre.getContenu(), newOffre.getCompetences(), newOffre.getEntrepriseName()));
         return newOffre;
-    }
-
-    @POST
-    @Consumes("application/json")
-    @Path("/create")
-    public void create(final MyJaxBean input) {
-        System.out.println("param1 = " + input.param1);
-        System.out.println("param2 = " + input.param2);
     }
 }
